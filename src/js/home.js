77dd81d908c8460fa1a43c473435d27f8f77cc2a
MLoydd <mike.osmand@gmail.com>
2016-05-06 19:58:14 +0200

    first commit

---
+++ src/js/home.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Mike on 5/6/2016.
+ */
+
+$(document).ready(function() {
+    $('#pagepiling').pagepiling({
+        direction: 'horizontal',
+        verticalCentered: true,
+        sectionsColor: [],
+        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+        menu: '#myMenu',
+        scrollingSpeed: 700,
+        easing: 'swing',
+        loopBottom: false,
+        loopTop: false,
+        css3: true,
+        navigation: {
+            'textColor': '#000',
+            'bulletsColor': '#000',
+            'position': 'right',
+            'tooltips': ['section1', 'section2', 'section3', 'section4']
+        },
+        normalScrollElements: null,
+        normalScrollElementTouchThreshold: 5,
+        touchSensitivity: 5,
+        keyboardScrolling: true,
+        sectionSelector: '.section',
+        animateAnchor: false,
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){}
+    });
+});
